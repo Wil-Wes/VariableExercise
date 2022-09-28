12542154daff865115263d9ba2cfe2ee1a3767bd
Wil-Wes <will.d.wes@gmail.com>
2022-09-28 09:05:54 -0500

    Completed Variable Exercise

---
+++ script.js
@@ -0,0 +1,43 @@
+//1 
+//const
+
+//2
+let num = 20;
+
+//3 
+num = 15;
+
+//4
+num += 7;
+//num = num + 7;
+
+//5 
+num--;
+
+//6
+num = "This is not a number";
+
+//7
+let blank;
+
+//8
+console.log(blank);
+
+//9
+blank = null;
+console.log(blank);
+
+// Bonus 
+// 1
+const quarter = 0.25, dime = 0.10, nickel = .05, penny = .01;
+
+// const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.5, 0.01];
+//2
+console.log(quarter, dime, nickel, penny);
+
+//3
+let letters = "qwertyuiop";
+
+//4
+console.log(letters[9]);
+// console.log(letters[letters.length - 1]);
